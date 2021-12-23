ea7fa6521fe79d7ddaaf044fc01247952acb6ea9
Ilona Kedracka <ilona.kedracka@gmail.com>
2021-12-23 11:22:05 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Page() {
 
       <main className="content">
         <Hero
-          title="Get Started with Headless WordPress"
+          title="Get Started with Headless WordPress development"
           buttonText="Developer Docs"
           buttonURL="https://faustjs.org"
           button2Text="Headless on GitHub"
